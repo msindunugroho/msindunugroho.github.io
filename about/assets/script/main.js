4ca1aee16e14b8b32ac174e8fdd52f027fe14ba4
msindunugroho <m.sindunugroho@gmail.com>
2023-02-26 15:00:00 +0700

    config

---
+++ about/assets/script/main.js
@@ -0,0 +1,20 @@
+// import validator from 'validator';
+// // https://formspree.io/f/mknanzgy
+// // post
+// const submit = document.querySelector('#submit');
+// alert('test');
+
+
+// submit.addEventListener('click', function(event) {
+//     event.preventDefault();
+//     const inputName = document.querySelector('#name').value;
+//     const inputEmail = document.querySelector('#email').value;
+//     const inputMsg = document.querySelector('#message').value;
+
+//     const email = validator.isEmail(inputEmail);
+
+//     console.log(inputName, inputEmail, inputMsg);
+//     console.log(email);
+// })
+
+
