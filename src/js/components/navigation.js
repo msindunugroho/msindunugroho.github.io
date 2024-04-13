76a4faabcd28db837fbe3807ea23ddc06aac87c7
msindunugroho <m.sindunugroho@gmail.com>
2024-04-13 23:19:22 +0700

    remake navigation structures

--- src/js/components/navigation.js
+++ src/js/components/navigation.js
@@ -1,8 +1,10 @@
 export const navigation = () => {
     const menuToggle = document.querySelector('.menu_toggle');
     const navBar = document.querySelector('.nav_bar');
+    const spanMenu = menuToggle.querySelectorAll('span')
 
     menuToggle.addEventListener('click', () => {
         navBar.classList.toggle('show_nav-bar');
+        spanMenu.forEach( (span) => span.classList.toggle('active'))
     })
 }
